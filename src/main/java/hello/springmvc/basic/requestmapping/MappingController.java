04de9a02f10251cce673cf895582cffc1cdab13e
Angela-OH <wisdom990629@likelion.org>
2023-01-25 01:32:43 +0900

    MVC basic functionality

---
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,40 @@
+package hello.springmvc.basic.requestmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MappingController {
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping(value = "/hello-basic") // url로 매핑
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET) // url로 매핑
+    public String mappingGetV1() {
+        log.info("mappingGetV1");
+        return "ok";
+    }
+
+    @GetMapping("/mapping-get-v2")
+    public String mappingGetV2() {
+        log.info("mappingGetV2");
+        return "ok";
+    }
+
+    /**
+     * PathVariable 사용
+     * 변수명이 같으면 생략 가능
+     * @PathVariable("userId") String userId -> @PathVariable userId
+     * /mapping/userA
+     */
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userId = {}", data);
+        return "ok";
+    }
+}
